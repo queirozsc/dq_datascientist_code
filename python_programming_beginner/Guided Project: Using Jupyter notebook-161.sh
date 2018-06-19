85ee56db33641ce367a3f6aceb119dc36acdc521
SERGIO QUEIROZ <queirozsc@gmail.com>
2018-06-19 20:03:16 -0300

    Python programming: beginner

---
+++ python_programming_beginner/Guided Project: Using Jupyter notebook-161.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+## 1. How Guided Projects Work ##
+
+
+
+
+## 2. Displaying Output ##
+
+
+
+
+## 3. Execution order ##
+
+
+
+
+## 4. Editing cells ##
+
+
+
+
+## 5. Markdown cells ##
+
+
+
+
+## 6. Saving and downloading ##
+
+
+
+
+## 7. Next Steps ##
+
+
